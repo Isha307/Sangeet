5cc0622465eeceab96a3788e0c09e466951023e3
Isha307 <56998032+Isha307@users.noreply.github.com>
2022-04-23 22:06:12 +0530

    x

--- Sangeet/urls.py
+++ Sangeet/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
+
 urlpatterns = [
     path('songs',views.songs, name='songs'),
     path('songs/<int:id>',views.songpost, name='songpost'),
@@ -10,5 +14,7 @@ urlpatterns = [
     path('history', views.history, name='history'),
     path('c/<str:channel>', views.channel, name='channel'),
     path('upload', views.upload, name='upload'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
--- sangeet1/settings.py
+++ sangeet1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'yk_al!5t*sxttlx2l%)qzs!#=1)x*1^%%b&wkvo-um7$6693^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
